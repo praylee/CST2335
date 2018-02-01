ca825b2a8a7f2cca13ca875fc15e7a19e97dfc10
praylee <prayleee@gmail.com>
2018-02-01 01:21:19 -0500

    Lab 3 code finish, commit

--- app/src/main/java/com/example/lee/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/lee/androidlabs/LoginActivity.java
@@ -1,19 +1,44 @@
 package com.example.lee.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class LoginActivity extends Activity {
-
-    Button button= (Button)(findViewById(R.id.imageButton2));
+    public static final String prefDefaultEmail = "DefaultEmail";
+    SharedPreferences preferences;
+    Button loginButton;
+    TextView editLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        preferences = getSharedPreferences(prefDefaultEmail, Context.MODE_PRIVATE);
+        loginButton= (Button)(findViewById(R.id.buttonLogin));
+        editLogin = (TextView) (findViewById(R.id.editLogin));
         Log.i("LoginActivity", "in onCreate()");
+        String loginName = preferences.getString(prefDefaultEmail,"prayleee@gmail.com");
+        editLogin.setText(loginName);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(prefDefaultEmail,editLogin.getText().toString());
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
     }
 
     @Override
