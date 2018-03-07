98bc275e9106c5c2dc46da744e89948338b23267
praylee <prayleee@gmail.com>
2018-03-06 23:25:55 -0500

    Lab 5 code finish, add database control, commit

---
+++ app/src/main/java/com/example/lee/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.lee.androidlabs;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by LEE on 2018-02-11.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Messages.db";
+    private static final int VERSION_NUM = 1;
+
+    protected static final String TABLE_NAME = "MESSAGETABLE";
+    protected static final String KEY_ID = "ID";
+    protected static final String KEY_MESSAGE = "MESSAGE";
+
+    public ChatDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+        sqLiteDatabase.execSQL("CREATE TABLE "
+                + TABLE_NAME + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_MESSAGE
+                + " TEXT" + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVer, int newVer) {
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVer + "newVersion=" + newVer);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
